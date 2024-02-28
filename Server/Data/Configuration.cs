632f59527ad8af08fe293379449f1fe8a1ad721c
Peter Overeijnder <peterovereijnder@hotmail.com>
2024-02-28 20:16:47 +0100

    -

--- Server/Data/Configuration.cs
+++ Server/Data/Configuration.cs
@@ -3,6 +3,16 @@
     internal class Configuration
     {
         public int Port { get; set; }
-        public string DbcType { get; set; }
+        public Database Dbc { get; set; }
+
+        internal class Database
+        {
+            public string Type { get; set; }
+            public string IP { get; set; }
+            public int Port { get; set; }
+            public string Db { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
     }
 }
