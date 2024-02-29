9ea179aaea3c7f6ef828d47ce1759a69222bf200
Peter Overeijnder <peterovereijnder@hotmail.com>
2024-02-29 13:52:51 +0100

    -

--- Server/Data/Configuration.cs
+++ Server/Data/Configuration.cs
@@ -2,17 +2,43 @@
 {
     internal class Configuration
     {
+        #region Properties
         public int Port { get; set; }
         public Database Dbc { get; set; }
+        #endregion //Properties
 
+        #region Constructors
+        public Configuration()
+        {
+            this.Port = 0;
+            this.Dbc = new Database();
+        }
+        #endregion //Constructors
+
+        #region Classes
         internal class Database
         {
+            #region Properties
             public string Type { get; set; }
             public string IP { get; set; }
             public int Port { get; set; }
             public string Db { get; set; }
             public string Username { get; set; }
             public string Password { get; set; }
+            #endregion //Properties
+
+            #region Constructors
+            public Database()
+            {
+                this.Type = "";
+                this.IP = "";
+                this.Port = 0;
+                this.Db = "";
+                this.Username = "";
+                this.Password = "";
+            }
+            #endregion //Constructors
         }
+        #endregion //Classes
     }
 }
